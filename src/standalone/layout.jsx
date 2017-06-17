6d6792ba8d88b39516960ceef47d870b34c95f7d
Unknown <npraiwan@outlook.com>
2017-06-18 00:48:50 +0700

    rebuild and set swagger-ui style class from outside

--- src/standalone/layout.jsx
+++ src/standalone/layout.jsx
@@ -27,7 +27,7 @@ export default class StandaloneLayout extends React.Component {
 
     return (
 
-      <Container className='swagger-ui'>
+      <Container>
         { Topbar ? <Topbar/> : null }
         { loadingStatus === "loading" &&
           <div className="info">
